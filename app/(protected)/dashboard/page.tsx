ae47b1bffbf2860386c25daaebebafe39bac4777
irfan <ferdiansyahirfan32@gmail.com>
2026-01-31 10:52:44 +0700

    start editing dashboard page

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,13 +1,21 @@
 import { SidebarCollapseButton } from "@/components/ui/SidebarCollapseButton";
+import OverviewCards from "./(components)/OverviewCards";
 
 const page = async () => {
   return (
-    <>
-      <h1>Dashboard</h1>
+    <div className="w-full min-h-screen p-4">
+      <header>
+        <h1>Dashboard</h1>
+        <p className="text-muted-foreground">
+          Overview of your inventory and financial performance
+        </p>
+      </header>
+      <OverviewCards />
+      {/* Toggle Button */}
       <div className="bottom-10 absolute md:hidden lg:hidden">
         <SidebarCollapseButton mode="mobile" />
       </div>
-    </>
+    </div>
   );
 };
 
