9f4b9f59de3743e56102efae8857411e2b5fdad7
iirfanferdian <ferdiansyahirfan32@gmail.com>
2026-02-02 14:21:50 +0700

    styling inventory-page

---
+++ app/(protected)/inventory/(components)/FilterDropdown.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown, Filter } from "lucide-react";
+
+export function FilterDropdown() {
+  const [showStatusBar, setShowStatusBar] = React.useState(true);
+  const [showActivityBar, setShowActivityBar] = React.useState(false);
+  const [showPanel, setShowPanel] = React.useState(false);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="hover:bg-background bg-background text-sm text-foreground h-full flex rounded-md">
+          <Filter /> Filter <ChevronDown />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40">
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+          <DropdownMenuCheckboxItem
+            checked={showStatusBar ?? false}
+            onCheckedChange={setShowStatusBar}
+          >
+            Status Bar
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem
+            checked={showActivityBar}
+            onCheckedChange={setShowActivityBar}
+            disabled
+          >
+            Activity Bar
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem
+            checked={showPanel}
+            onCheckedChange={setShowPanel}
+          >
+            Panel
+          </DropdownMenuCheckboxItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- app/(protected)/inventory/page.tsx
+++ app/(protected)/inventory/page.tsx
@@ -1,11 +1,59 @@
+import { Button } from "@/components/ui/button";
 import { SidebarCollapseButton } from "@/components/ui/SidebarCollapseButton";
+import { Download, Plus, Search } from "lucide-react";
+import { FilterDropdown } from "./(components)/FilterDropdown";
+import { CategoriesDropdown } from "./(components)/CategoriesDropdown";
+import { ItemsTable } from "./(components)/ItemsTable";
+import { PaginationButton } from "./(components)/PaginationButton";
 
 const page = async () => {
   return (
     <>
-      <h1>Inventory Letsgoo</h1>
-      <div className="bottom-10 absolute md:hidden lg:hidden">
-        <SidebarCollapseButton mode="mobile" />
+      <div className="w-full min-h-screen p-4">
+        <header className="flex justify-between items-center">
+          <div>
+            <h1>Inventory</h1>
+            <p className="text-muted-foreground">
+              Manage your products and stock levels
+            </p>
+          </div>
+          <Button className="bg-primary hover:bg-primary/80 dark:text-foreground ">
+            <Plus /> Add New Item{" "}
+          </Button>
+        </header>
+
+        {/* Section */}
+        <section className="flex my-6 w-full">
+          <div className="flex w-3/4 items-center gap-4">
+            <div className="relative w-full">
+              <Search
+                size={16}
+                className="absolute left-2 top-1/2 -translate-y-1/2 text-muted-foreground"
+              />
+              <input
+                className="bg-background rounded-md pl-8 py-3 w-full outline-none ring-offset-2 focus:ring-primary focus:ring-2 text-muted-foreground text-sm"
+                placeholder="Search by name or SKU..."
+              />
+            </div>
+            <FilterDropdown />
+            <CategoriesDropdown />
+          </div>
+          <div className="flex items-center justify-end w-1/2">
+            <Button className="bg-primary dark:text-foreground text-primary-foreground">
+              <Download /> Export
+            </Button>
+          </div>
+        </section>
+        <ItemsTable />
+        <footer className="w-full flex justify-between my-3">
+          <p>Showing 1 to 5 of 8 items</p>
+          <div>
+            <PaginationButton />
+          </div>
+        </footer>
+        <div className="bottom-10 absolute md:hidden lg:hidden">
+          <SidebarCollapseButton className="fixed" mode="mobile" />
+        </div>
       </div>
     </>
   );
