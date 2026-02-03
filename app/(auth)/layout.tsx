f5c0135f6b7785d853ee9842d813b6ff7873de28
iirfanferdian <ferdiansyahirfan32@gmail.com>
2026-02-03 22:15:00 +0700

    fix design style in dark theme

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,14 +9,19 @@ export default function AuthLayout({
     <div className="w-full min-h-screen grid grid-cols-1 md:grid-cols-2">
       <aside className="hidden md:flex flex-col bg-primary justify-center items-start p-12">
         <div className="flex gap-2">
-          <Box size={40} className="text-primary-foreground" />
-          <h1 className="text-primary-foreground">Inventra</h1>
+          <Box
+            size={40}
+            className="text-primary-foreground dark:text-foreground"
+          />
+          <h1 className="text-primary-foreground dark:text-foreground">
+            Inventra
+          </h1>
         </div>
         <div className=" mt-6">
-          <h1 className="text-primary-foreground">
+          <h1 className="text-primary-foreground dark:text-foreground">
             Manage Your Inventory & Finance with Ease
           </h1>
-          <p className="text-secondary pt-2">
+          <p className="text-secondary dark:text-foreground pt-2">
             Streamline your business operations with our powerful inventory and
             financial management platform.
           </p>
@@ -25,11 +30,11 @@ export default function AuthLayout({
           {/* Real-time Analytics */}
           <li className="list-none flex justify-center items-center gap-3">
             <div className="bg-muted-foreground/40 p-2 rounded-xl">
-              <ChartColumn size={30} />
+              <ChartColumn size={30} className="dark:text-foreground" />
             </div>
             <div className="flex flex-col">
-              <h4>Real-time Analytics</h4>
-              <p className="m-0 opacity-90">
+              <h4 className="dark:text-foreground">Real-time Analytics</h4>
+              <p className="dark:text-foreground m-0 opacity-90">
                 Track your business performance with live dashboards
               </p>
             </div>
@@ -38,11 +43,11 @@ export default function AuthLayout({
           {/* Team Collaboration*/}
           <li className="list-none flex justify-center items-center gap-3">
             <div className="bg-muted-foreground/40 p-2 rounded-xl">
-              <Users size={30} />
+              <Users size={30} className="dark:text-foreground" />
             </div>
             <div className="flex flex-col">
-              <h4>Team Collaboration</h4>
-              <p className="m-0 opacity-90">
+              <h4 className="dark:text-foreground">Team Collaboration</h4>
+              <p className="m-0 opacity-90 dark:text-foreground">
                 Work together with role-based access controls
               </p>
             </div>
@@ -51,11 +56,11 @@ export default function AuthLayout({
           {/* Secure & Reliable */}
           <li className="list-none flex justify-center items-center gap-3">
             <div className="bg-muted-foreground/40 p-2 rounded-xl">
-              <Shield size={30} />
+              <Shield size={30} className="dark:text-foreground" />
             </div>
             <div className="flex flex-col">
-              <h4>Secure & Reliable</h4>
-              <p className="m-0 opacity-90">
+              <h4 className="dark:text-foreground">Secure & Reliable</h4>
+              <p className="m-0 opacity-90 dark:text-foreground">
                 Enterprise-grade security for your data
               </p>
             </div>
