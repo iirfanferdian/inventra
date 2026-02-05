a55cac18269ff96d158f9014de5930a113bdaaa4
iirfanferdian <ferdiansyahirfan32@gmail.com>
2026-02-05 13:08:02 +0700

    make the form of add new item works

--- app/(protected)/inventory/(components)/AddNewItemButton.tsx
+++ app/(protected)/inventory/(components)/AddNewItemButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -17,141 +18,230 @@ import {
   Select,
   SelectContent,
   SelectItem,
-  SelectSeparator,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
 import { Plus } from "lucide-react";
 
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+
 export function NewItemButton() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm<IAddItem>({ mode: "onSubmit", criteriaMode: "firstError" });
+
   const Categories = [
-    { label: "Electronics", value: "en" },
-    { label: "Audio", value: "es" },
-    { label: "Accessories", value: "fr" },
-    { label: "Monitors", value: "de" },
-    { label: "Storage", value: "it" },
-    { label: "Networking", value: "zh" },
+    { label: "Electronics", value: "electronics" },
+    { label: "Audio", value: "audio" },
+    { label: "Accessories", value: "accessories" },
+    { label: "Monitors", value: "monitors" },
+    { label: "Storage", value: "storage" },
+    { label: "Networking", value: "networking" },
   ] as const;
 
-  return (
-    <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button className="bg-primary hover:bg-primary/80 dark:text-foreground ">
-            <Plus />
-            Add New Item
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-lg">
-          <DialogHeader>
-            <DialogTitle>Add New Item</DialogTitle>
-            <DialogDescription>
-              Fill in the details to add a new item to your inventory.
-            </DialogDescription>
-          </DialogHeader>
+  interface IAddItem {
+    item: string;
+    code: string;
+    description?: string;
+    category?: string;
+    stock: number;
+    minstock?: number;
+    price: number;
+  }
 
+  const onSubmit: SubmitHandler<IAddItem> = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Dialog
+      onOpenChange={(open) => {
+        if (!open) {
+          reset();
+        }
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button className="bg-primary hover:bg-primary/80 dark:text-foreground ">
+          <Plus />
+          Add New Item
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-lg">
+        <DialogHeader>
+          <DialogTitle>Add New Item</DialogTitle>
+          <DialogDescription>
+            Fill in the details to add a new item to your inventory.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit(onSubmit)}>
           {/* Input */}
           <FieldGroup>
+            {/* Name & Code */}
             <div className="grid grid-cols-2 gap-3">
               <Field>
                 <Label htmlFor="item">Item Name*</Label>
-                <Input id="item" name="item" placeholder="MacBook Pro 14" />
+                <Input
+                  id="item"
+                  placeholder="MacBook Pro 14"
+                  type="text"
+                  {...register("item", {
+                    required: {
+                      value: true,
+                      message: "Please fill out this field",
+                    },
+                  })}
+                />
+                {errors?.item?.message && (
+                  <p className="leading-3 text-sm text-red-500">
+                    {errors.item.message}
+                  </p>
+                )}
               </Field>
               <Field>
-                <Label htmlFor="sku">SKU/Code*</Label>
-                <Input id="sku" name="sku" placeholder="MBP-14 M3" />
+                <Label htmlFor="code">SKU/Code*</Label>
+                <Input
+                  id="code"
+                  placeholder="MBP-14 M3"
+                  {...register("code", {
+                    required: {
+                      value: true,
+                      message: "Please fill out this field",
+                    },
+                  })}
+                />
+                {errors?.code?.message && (
+                  <p className="leading-3 text-sm text-red-500">
+                    {errors.code.message}
+                  </p>
+                )}
               </Field>
             </div>
+
+            {/* Desc */}
             <Field>
               <Label htmlFor="description">Description</Label>
 
               <InputGroup>
                 <InputGroupTextarea
-                  id="form-rhf-demo-description"
+                  id="description"
                   placeholder="Brief description of the item..."
                   rows={4}
                   className="min-h-24 resize-none"
                 />
               </InputGroup>
             </Field>
-            <Field>
-              {/* Select Button */}
-              <Select>
-                <Label htmlFor="categories">Category</Label>
 
-                <SelectTrigger
-                  id="form-rhf-select-language"
-                  className="min-w-[120px]"
-                >
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="item-aligned">
-                  <SelectSeparator />
-                  {Categories.map((category) => (
-                    <SelectItem key={category.value} value={category.value}>
-                      {category.label}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+            {/* Select Category Button */}
+            <Field>
+              <Label>Category</Label>
+              <Controller
+                control={control}
+                name="category"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select Category" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {Categories.map((cat) => (
+                        <SelectItem key={cat.value} value={cat.value}>
+                          {cat.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                )}
+              />
             </Field>
 
             {/* Footer Input */}
-
             <div className="grid grid-cols-3 gap-3">
+              {/* Field Stock */}
               <Field>
-                <Label htmlFor="item">Stock*</Label>
+                <Label htmlFor="stock">Stock*</Label>
                 <Input
-                  id="item"
-                  name="item"
+                  id="stock"
                   type="number"
-                  defaultValue={1}
-                  min={0}
+                  placeholder="0"
+                  {...register("stock", {
+                    required: "Stock is required",
+                    valueAsNumber: true,
+                    min: { value: 0, message: "Stock cannot be negative" },
+                  })}
                   onKeyDown={(e) => {
-                    if (e.key === "-" || e.key === "e") {
-                      e.preventDefault();
-                    }
+                    if (e.key === "-" || e.key === "e") e.preventDefault();
+                  }}
+                />
+                {errors?.stock?.message && (
+                  <p className="leading-3 text-sm text-red-500">
+                    {errors.stock.message}
+                  </p>
+                )}
+              </Field>
+
+              {/* Field Min Stock */}
+              <Field>
+                <Label htmlFor="minStock">Min Stock</Label>
+                <Input
+                  id="minStock"
+                  type="number"
+                  placeholder="5"
+                  {...register("minstock", {
+                    valueAsNumber: true,
+                    min: { value: 0, message: "Min Stock cannot be negative" },
+                  })}
+                  onKeyDown={(e) => {
+                    if (e.key === "-" || e.key === "e") e.preventDefault();
                   }}
                 />
               </Field>
+
+              {/* Field Price */}
               <Field>
-                <Label htmlFor="sku">Min Stock</Label>
+                <Label htmlFor="price">Price*</Label>
                 <Input
-                  id="sku"
-                  name="sku"
+                  id="price"
                   type="number"
-                  min={0}
+                  placeholder="100000"
+                  {...register("price", {
+                    required: "Price is required",
+                    valueAsNumber: true,
+                    min: { value: 0, message: "Price must be at least 0" },
+                  })}
                   onKeyDown={(e) => {
-                    if (e.key === "-" || e.key === "e") {
-                      e.preventDefault();
-                    }
+                    if (e.key === "-" || e.key === "e") e.preventDefault();
                   }}
                 />
-              </Field>
-              <Field>
-                <Label htmlFor="sku">Price</Label>
-                <Input
-                  id="sku"
-                  name="sku"
-                  type="number"
-                  min={0}
-                  onKeyDown={(e) => {
-                    if (e.key === "-" || e.key === "e") {
-                      e.preventDefault();
-                    }
-                  }}
-                />
+                {errors?.price?.message && (
+                  <p className="leading-3 text-sm text-red-500">
+                    {errors.price.message}
+                  </p>
+                )}
               </Field>
             </div>
           </FieldGroup>
-          <DialogFooter>
+          <DialogFooter className="mt-4">
             <DialogClose asChild>
               <Button variant="outline">Cancel</Button>
             </DialogClose>
-            <Button type="submit">Save changes</Button>
+            <Button
+              type="submit"
+              variant="outline"
+              className="bg-primary text-primary-foreground dark:bg-primary dark:text-foreground hover:bg-primary/80 hover:text-primary-foreground dark:hover:bg-primary/80 dark:hover:text-foreground"
+            >
+              Save changes
+            </Button>
           </DialogFooter>
-        </DialogContent>
-      </form>
+        </form>
+      </DialogContent>
     </Dialog>
   );
 }
