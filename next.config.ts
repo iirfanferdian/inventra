7a43c36b8418c00dd26cdb7618f51fa00326c4a7
iirfanferdian <ferdiansyahirfan32@gmail.com>
2026-02-18 11:04:47 +0700

    first init of finalizing the settings feature

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["@prisma/client", "@prisma/adapter-pg", "pg"],
-  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
